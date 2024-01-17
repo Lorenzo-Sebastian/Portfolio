89c8b1dd7024b5996edd96cc7ded6b68cac62132
Lorenzo-Sebastian <156223272+Lorenzo-Sebastian@users.noreply.github.com>
2024-01-17 10:50:10 -0600

    for loop

--- assign1.js
+++ assign1.js
@@ -1,8 +1,14 @@
 //Lorenzo Sebastian 1/10/2024
 var x = 5;
-var h = prompt("give me a value");
+var h = prompt("enter your favorite beverage");
 alert(h);
 i += 3;
 var i = parseInt(prompt("give me a number"));
 window.alert(i);
-console.log("");
+console.log("your favorite drink is "+ h + " your number is "+ i);
+//for loop
+let sum = 0;
+for (let i = 1; i <=5; i++){
+    sum += i;
+}
+alert(sum);
