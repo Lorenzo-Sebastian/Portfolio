04c825191827b4a94115a687e92526326460fda0
Lorenzo-Sebastian <156223272+Lorenzo-Sebastian@users.noreply.github.com>
2024-01-17 11:11:30 -0600

    asgn

--- assign1.js
+++ assign1.js
@@ -1,10 +1,8 @@
 //Lorenzo Sebastian 1/10/2024
 var x = 5;
 var h = prompt("enter your favorite beverage");
-alert(h);
-i += 3;
+i = 3;
 var i = parseInt(prompt("give me a number"));
-
 console.log(h + i);
 //for loop
 //let sum = 0;
