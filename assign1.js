ad3a679938db4c8f099475b6eff0885bae428a38
Lorenzo-Sebastian <156223272+Lorenzo-Sebastian@users.noreply.github.com>
2024-01-17 10:27:17 -0600

    console.log

--- assign1.js
+++ assign1.js
@@ -1,2 +1,9 @@
 //Lorenzo Sebastian 1/10/2024
 var x = 5;
+var h = prompt("give me a value");
+alert(h);
+i += 3;
+var i = parseInt(prompt("give me a number"));
+window.alert(i)
+console.log("");
+window.location
