420dd57d106f75993a12a2365a0241a6f8cb8c21
Lorenzo-Sebastian <156223272+Lorenzo-Sebastian@users.noreply.github.com>
2024-01-17 11:31:35 -0600

    consol.

--- assign1.js
+++ assign1.js
@@ -2,7 +2,7 @@
 var h = prompt("enter your favorite beverage");
 var i = 3
 var i = parseInt(prompt("give me a number"));
-console.log("hi");
+console.log("hi")
 //for loop
 //let sum = 0;
 //for (let i = 1; i <=5; i++){
