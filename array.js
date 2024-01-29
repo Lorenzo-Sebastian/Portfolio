b1d5e6a1c44060c6d321ab052804e0dc63c66878
Lorenzo-Sebastian <156223272+Lorenzo-Sebastian@users.noreply.github.com>
2024-01-28 21:20:12 -0600

    step 5

--- array.js
+++ array.js
@@ -5,3 +5,14 @@ let add = function(num1,num2)
 }
 let result = add(5,6);
 console.log(result);
+
+function fucntionName (functionparameter,number)
+{if ((number % 2)==0){
+    functionparameter(number);
+}
+else{
+    console.log("the number is odd");
+}
+
+}
+let results = functionName (3);
