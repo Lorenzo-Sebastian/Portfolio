c4f382ff346530c19fcf6e07732681de1510facd
Lorenzo-Sebastian <156223272+Lorenzo-Sebastian@users.noreply.github.com>
2024-02-04 17:40:45 -0600

    jdj

--- domasgn.js
+++ domasgn.js
@@ -5,13 +5,13 @@ for (var i = 1; i <= 5; i ++){
  const newParagraph = document.createElement("p");
  const text = document.createTextNode("middle paragraph");
  newParagraph.appendChild(text);
- 
+
 }
 
 const body = document.querySelector('body');
 const button = document.querySelector('button');
 const colors = ['red', 'green', 'yellow'];
-body.style.backgroundColor='violet';
+//body.style.backgroundColor='violet';
 button.addEventListener('click',function(){
     const colorIndex=parseInt(Math.random()*colors.length);
     body.style.backgroundColor=colors[colorIndex];
