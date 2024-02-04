aa262d3906471b3e355f706dc2eae368c65b02b6
Lorenzo-Sebastian <156223272+Lorenzo-Sebastian@users.noreply.github.com>
2024-02-04 15:36:20 -0600

    color

--- domasgn.js
+++ domasgn.js
@@ -1,1 +1,7 @@
-var i = document.getElementById(div);
+var i = document.querySelector('div');
+
+function color(z){
+    var a = getComputedStyle(z);
+    var b = a.backgroundColor;
+    document.getElementsByName("BODY")[0].style.backgroundColor=b;
+}
