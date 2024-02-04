55e7b6fd473af8dbf1b663b91b34ee8ae5f42d38
Lorenzo-Sebastian <156223272+Lorenzo-Sebastian@users.noreply.github.com>
2024-02-04 15:57:25 -0600

    help

--- domasgn.js
+++ domasgn.js
@@ -1,4 +1,11 @@
 var i = document.querySelector('div');
+let btnSend = document.querySelector('button');
+let message = document.querySelector('p');
+
+btnSend.addEventListener('click,'() ==
+{
+message.innerText= 'here is some new text';
+})
 
 function color(z){
     var a = getComputedStyle(z);
