7be7843af1100f6d6d20eb5e61a275d3d256f974
Lorenzo-Sebastian <156223272+Lorenzo-Sebastian@users.noreply.github.com>
2024-02-04 16:13:05 -0600

    newfunction

--- domasgn.js
+++ domasgn.js
@@ -5,6 +5,7 @@ let message = document.querySelector('p');
 const body = document.querySelector('body');
 const button = document.querySelector('button');
 const colors = ['red', 'green', 'yellow'];
+body.style.backgroundColor='violet';
 button.addEventListener('click',function(){
     const colorIndex=parseInt(Math.random()*colors.length);
     body.style.backgroundColor=colors[colorIndex];
