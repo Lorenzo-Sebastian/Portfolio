d447b6b7480c79002eeb7f71cec6ec5b265a1e57
Lorenzo-Sebastian <156223272+Lorenzo-Sebastian@users.noreply.github.com>
2024-02-07 15:18:11 -0600

    js changes

--- domasgn.js
+++ domasgn.js
@@ -13,12 +13,14 @@ function colorBlack()
     document.body.style.background="black";
     document.querySelectorAll('h3').forEach(e=>e.style.color="white");
     document.querySelectorAll('p').forEach(e=>e.style.color="white");
+    document.querySelectorAll('label').forEach(e=>e.style.color="white");
 }
 function colorWhite()
 {
     document.body.style.background="white";
     document.querySelectorAll('h3').forEach(e=>e.style.color="black");
     document.querySelectorAll('p').forEach(e=>e.style.color="black");
+    document.querySelectorAll('label').forEach(e=>e.style.color="black");
 }
 
 const txt1 = document.getElementById('tbuser');
