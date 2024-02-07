cd5a9ffb6c2c9063a91a0ae3722eb8b0b4242797
Lorenzo-Sebastian <156223272+Lorenzo-Sebastian@users.noreply.github.com>
2024-02-07 14:57:46 -0600

    projecttest

--- domasgn.js
+++ domasgn.js
@@ -3,7 +3,7 @@
 var para = document.querySelectorAll("h3");
 for (var i = 1; i <= 5; i ++){
     var newParagraph = document.createElement("p");
-    var text = document.createTextNode("middle paragraph");
+    var text = document.createTextNode("Loop and modifying elements!");
     newParagraph.appendChild(text);
     para[0].parentElement.appendChild(newParagraph);
 }
@@ -15,3 +15,11 @@ function colorBlack()
     document.querySelectorAll('p').forEach(e=>e.style.color="white");
 }
 
+const txt1 = document.getElementById('tbuser');
+const btn1 = document.getElementById('btn1');
+const out1 = document.getElementById('output1');
+
+function fun1(){
+    out1.innerHTML = txt1.value;
+}
+btn1.addEventListener('click',fun1);
