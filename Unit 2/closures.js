7a07dc19fbec1be507ad9d4671faf67da38342a6
Lorenzo-Sebastian <156223272+Lorenzo-Sebastian@users.noreply.github.com>
2024-02-28 10:23:29 -0600

    return

--- Unit 2/closures.js
+++ Unit 2/closures.js
@@ -2,14 +2,14 @@ function createPlayer(hp, name, move){
     var hp = hp;
     var name = name;
     var move = move;
-    return function () {
-        return hp;
-        return (name);
-        for (var i = 1; i <= 5; i ++){
-
+     return{
+        getName(){
+            return (name);
+            return hp;
         }
+     }
     }
-}
+
 
 var results = createPlayer(150,"Richtofen", 300 )
-console.log(results());
+console.log(results);
