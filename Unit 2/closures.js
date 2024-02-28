54dff9927f8c1ed1b012714612bd908596bdcfd0
Lorenzo-Sebastian <156223272+Lorenzo-Sebastian@users.noreply.github.com>
2024-02-28 11:07:19 -0600

    comp

--- Unit 2/closures.js
+++ Unit 2/closures.js
@@ -1,14 +1,30 @@
-function createPlayer(hp, name, move){
-    var hp = hp;
+function createPlayer(name, hp, move){
     var name = name;
+    var hp = hp;
     var move = move;
     return {
-    
+
         getName()
         {
             return(name);
+               
+        }
+        ,getHp(){
+            return (hp)
         }
     }
+    
+    
 }
-var lonzo = createPlayer(150, "richtofen", 9);
-console.log(lonzo);
+
+var para = document.querySelectorAll("div");
+for (var i = 1; i <= 3; i ++){
+    var newParagraph = document.createElement("p");
+    var text = document.createTextNode("9");
+    newParagraph.appendChild(text);
+    para[0].parentElement.appendChild(newParagraph);
+}
+
+var lonzo = createPlayer("richtofen", 9, 9);
+console.log(lonzo.getName());
+console.log(lonzo.getHp());
