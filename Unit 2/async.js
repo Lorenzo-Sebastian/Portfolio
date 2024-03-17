212fe6edbcb025a58591f1bb625c1417606cb4cd
Lorenzo-Sebastian <156223272+Lorenzo-Sebastian@users.noreply.github.com>
2024-03-17 12:24:46 -0500

    async

---
+++ Unit 2/async.js
@@ -0,0 +1,21 @@
+async function run(){
+    var runner1 = false;
+    var runner2 = false;
+    var loser = "";
+
+    var runner1Now = new Promise(function(resolve){
+        setTimeout(()=>{
+            loser = "runner1";
+            resolve("runner1");
+
+        }, 9000);
+        
+    });
+    var runner2Now = new Promise(function(resolve){
+        setTimeout(()=>{
+            loser = "runner2";
+            resolve("runner2");
+        }, 8000);
+    });
+    return [await runner1Now, await runner2Now, loser];
+};
