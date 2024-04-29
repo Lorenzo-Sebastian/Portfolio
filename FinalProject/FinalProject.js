cb4921d2fe4e2a093b0cd08d06d131a29ec57c0e
Lorenzo-Sebastian <156223272+Lorenzo-Sebastian@users.noreply.github.com>
2024-04-29 12:09:41 -0500

    update

--- FinalProject/FinalProject.js
+++ FinalProject/FinalProject.js
@@ -109,15 +109,29 @@ function moveText(x, y) {
         initText2();
     }
 }
-
+var dom2, x2, y2, finalx2 = 100, finaly2 = 0;
 function initText2() {
-    var dom2, x2, y2, finalx2 = 100, finaly2 = 0;
+    
     dom2 = document.getElementById('theText').style;
     x2 = dom2.left;
     y2 = dom2.top;
     x2 = x2.match(/\d+/);
     y2 = y2.match(/\d+/);
-    
+    moveTextback(x2, y2);
 }
 
+function moveTextback(x2, y2) {
+    if (x2 != finalx2)
+        if (x2 > finalx2) x2--;
+        else if (x2 < finalx2) x2++;
 
+    if (y2 != finaly2)
+        if (y2 > finaly2) y2--;
+        else if (y2 < finaly2) y2++;
+
+    if ((x2 != finalx2) || (y2 != finaly2)) {
+        dom2.style.left = finalx2 + "px";
+        dom2.style.top = finaly2 + "px";
+        setTimeout("moveText(" + x2 + "," + y2 + ")", 1);
+    }
+}
